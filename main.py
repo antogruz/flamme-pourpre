83444bf0bd201a10b0ad861ba58ea438438f4fb4
Antoine Gruzelle <antogruz@gmail.com>
2021-02-25 18:09:52 +0100

    events

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import tkinter as tk
 def main():
     window = tk.Tk()
     window.title("Tac first window")
-    allWidgets(window)
-    frames(window)
+    #commands(window)
+    clicks(window)
     window.mainloop()
 
 def allWidgets(window):
@@ -28,6 +28,36 @@ def frames(window):
     cards = []
     for i in range(2, 6):
         cards.append(tk.Button(bot, text = str(i), bg = "green").pack(side = "left"))
+
+
+def grid(window):
+    for name, i in zip(["Rouleur", "Sprinteur"], [0, 1]):
+        a = 0
+        tk.Checkbutton(window, text = name, variable = a, onvalue = 1, offvalue = 0).grid(row = i)
+
+
+def commands(window):
+    def addLabel():
+        tk.Label(window, text = "Ça vient de sortir!").pack()
+
+    tk.Button(window, text = "C'est quoi flamme écarlate ?", command = addLabel).pack()
+
+def clicks(window):
+    def leftClick(e):
+        label("À Gauche !")
+
+    def rightClick(e):
+        label("À Droite !")
+
+    def middleClick(e):
+        label("Au milieu !")
+
+    def label(text):
+        tk.Label(window, text = text).pack()
+
+    window.bind("<Button-1>", leftClick)
+    window.bind("<Button-2>", middleClick)
+    window.bind("<Button-3>", rightClick)
 
 
 
