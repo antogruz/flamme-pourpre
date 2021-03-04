ca625798d70cceb77f85544e8d4dcb8b2f281b4e
Antoine Gruzelle <antogruz@gmail.com>
2021-03-04 15:33:57 +0100

    Riders

--- run.py
+++ run.py
@@ -11,21 +11,50 @@ def main():
 
 def run(window):
     trackWidgets = createTrack(window)
-    addRiders(trackWidgets)
+    riders = createRiders()
+    setPositions(riders)
+    displayRiders(trackWidgets, riders)
 
-def addRiders(widgets):
-    addRouleur(widgets[5][1], "green")
-    addRouleur(widgets[6][1], "red")
-    addRouleur(widgets[4][1], "blue")
-    addRouleur(widgets[5][0], "black")
-    addSprinteur(widgets[8][1], "green")
+def displayRiders(track, riders):
+    for rider in riders:
+        rider.display(track[rider.square][rider.lane])
+
+class Rider():
+    def __init__(self, color, display):
+        self.color = color
+        self.text = display
+        self.square = 0
+        self.lane = 0
+
+    def display(self, widget):
+        widget.config(text = self.text, fg = self.color)
+
+    def set(self, square, lane):
+        self.square = square
+        self.lane = lane
+
+def createRiders():
+    riders = []
+    riders.append(createRouleur("green"))
+    riders.append(createRouleur("red"))
+    riders.append(createRouleur("blue"))
+    riders.append(createRouleur("black"))
+    riders.append(createSprinteur("green"))
+    return riders
+
+def setPositions(riders):
+    riders[0].set(5, 1)
+    riders[1].set(6, 1)
+    riders[2].set(4, 1)
+    riders[3].set(5, 0)
+    riders[4].set(8, 1)
 
 
-def addRouleur(widget, color):
-    widget.config(text = "o±ỏ", fg = color)
+def createRouleur(color):
+    return Rider(color, "o±ỏ")
 
-def addSprinteur(widget, color):
-    widget.config(text = "o/ỏ", fg = color)
+def createSprinteur(color):
+    return Rider(color, "o/ỏ")
 
 main()
 
