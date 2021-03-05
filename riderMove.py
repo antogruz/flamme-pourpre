e35f5b42d0096af91500252d61ccc7c11bf9d22d
Antoine Gruzelle <antogruz@gmail.com>
2021-03-05 05:30:02 +0100

    roadtypes

--- riderMove.py
+++ riderMove.py
@@ -28,6 +28,26 @@ class RiderTest(Tester):
         self.rider.move(1, self.race)
         assert_equals((0, 0), self.rider.position())
 
+    def testDescent(self):
+        self.race.setAll("descent")
+        self.rider.move(1, self.race)
+        assert_equals((5, 0), self.rider.position())
+
+    def testEndOfRace(self):
+        self.race = Race(4)
+        self.rider.move(4, self.race)
+        assert_equals((3, 0), self.rider.position())
+
+    def testStartInAscent(self):
+        self.race.setAll("ascent")
+        self.rider.move(9, self.race)
+        assert_equals((5, 0), self.rider.position())
+
+    def testThroughAscent(self):
+        self.race.set("ascent", 1)
+        self.rider.move(9, self.race)
+        assert_equals((5, 0), self.rider.position())
+
 
 class Rider():
     def __init__(self, square, lane):
@@ -37,27 +57,58 @@ class Rider():
     def position(self):
         return (self.square, self.lane)
 
-    def move(self, n, race):
-        slot = (self.square + n, 0)
-        while not race.free(slot):
-            slot = previous(slot)
-        self.square, self.lane = slot
+    def move(self, distance, race):
+        distance = self.adaptDistanceToRoadType(distance, race)
+        self.square, self.lane = findAvailableSlot(race, self.square + distance)
 
+    def adaptDistanceToRoadType(self, distance, race):
+        starting = race.getRoadType(self.square)
+        if starting == "descent":
+            distance = max(distance, 5)
+        if containsAscent(race, self.square, self.square + distance):
+            distance = min(distance, 5)
+        return distance
+
+
+def findAvailableSlot(race, square):
+        slot = (square, 0)
+        while not (race.isFree(slot) and race.getRoadType(slot[0]) != "out") :
+            slot = previous(slot)
+        return slot
 
 def previous(slot):
     if slot[1] == 0:
         return (slot[0], 1)
     return (slot[0] - 1, 0)
 
+def containsAscent(race, start, end):
+    for i in range(start, end + 1):
+        if race.getRoadType(i) == "ascent":
+            return True
+    return False
 
 class Race():
-    def __init__(self):
+    def __init__(self, length = 100):
         self.obstacles = []
+        self.length = length
+        self.setAll("normal")
 
     def addRider(self, square, lane):
         self.obstacles.append((square, lane))
 
-    def free(self, slot):
+    def setAll(self, field):
+        self.field = [field for i in range(self.length)]
+
+    def set(self, field, square):
+        self.field[square] = field
+
+
+    def getRoadType(self, square):
+        if square >= self.length:
+            return "out"
+        return self.field[square]
+
+    def isFree(self, slot):
         return not slot in self.obstacles
 
 tests()
