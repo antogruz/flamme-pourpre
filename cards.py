14b70d4f7f701aa70a23b1d8b83a7d465b68a7ee
Antoine Gruzelle <antogruz@gmail.com>
2021-04-16 12:06:19 +0200

    cards

--- cards.py
+++ cards.py
@@ -7,23 +7,75 @@ def tests():
 
 class CardsTester(Tester):
     def testDrawAllDeck(self):
-        cards = Cards([1, 2, 3, 4], noop)
+        cards = Cards(deck(4), noop)
         cards.draw()
         assert_equals(0, cards.inDeck())
 
     def testNoDraw(self):
-        cards = Cards([1, 2, 3, 4], noop)
+        cards = Cards(deck(4), noop)
         assert_equals(4, cards.inDeck())
 
     def testDrawGet4Cards(self):
-        cards = Cards([1, 2, 3, 4, 5, 6], noop)
+        cards = Cards(deck(6), noop)
         drawn = cards.draw()
         assert_equals(4, len(drawn))
         assert_equals(2, cards.inDeck())
 
     def testDeckIsShuffled(self):
-        cards = Cards([6, 5, 4, 3, 2, 1], increasingOrder)
+        cards = Cards(deck(6), increasingOrder)
         assert_similars([1, 2, 3, 4], cards.draw())
+
+    def testCardPlayedIsOut(self):
+        cards = Cards(deck(4), noop)
+        cards.draw()
+        cards.play(1)
+        assert_similars([2, 3, 4], cards.draw())
+
+    def testShuffleOnlyAfterAllDeckDrawn(self):
+        cards = Cards(deck(8), increasingOrder)
+        cards.draw()
+        cards.play(1)
+        assert_similars([5, 6, 7, 8], cards.draw())
+
+    def testDraw4EvenWhen2CardsLeftInDeck(self):
+        cards = Cards(deck(6), increasingOrder)
+        cards.draw()
+        cards.play(2)
+        assert_similars([5, 6, 1, 3], cards.draw())
+
+    def testSeveralDiscards(self):
+        cards = Cards(deck(10), increasingOrder)
+        cards.draw()
+        cards.play(2)
+        cards.draw()
+        cards.play(5)
+        assert_similars([9, 10, 1, 3], cards.draw())
+        cards.play(9)
+        assert_similars([4, 6, 7, 8], cards.draw())
+        cards.play(4)
+        assert_similars([1, 3, 6, 7], cards.draw())
+
+
+    def testSeveralShuffles(self):
+        cards = Cards(deck(6), increasingOrder)
+        cards.draw()
+        cards.play(1)
+        cards.draw()
+        cards.play(5)
+        cards.draw()
+        cards.play(2)
+        assert_similars([3, 4, 6], cards.draw())
+
+    def test1card(self):
+        cards = Cards(deck(1), noop)
+        cards.draw()
+        cards.play(1)
+        assert_similars([], cards.draw())
+        assert_similars([], cards.discard)
+
+
+def deck(n):
+    return [ i for i in reversed(range(1, n + 1)) ]
 
 
 def noop(list):
@@ -35,13 +87,35 @@ def increasingOrder(list):
 class Cards():
     def __init__(self, deck, shuffle):
         self.deck = deck
+        self.discard = []
+        self.shuffle = shuffle
         shuffle(self.deck)
 
     def inDeck(self):
         return len(self.deck)
 
     def draw(self):
-        return [ self.deck.pop(0) for i in range(4) ]
+        self.hand = []
+        for i in range(4):
+            self.drawOne()
+        return self.hand
+
+    def drawOne(self):
+        if not self.deck:
+            self.deck = [ card for card in self.discard ]
+            self.shuffle(self.deck)
+            self.discard = []
+
+        if not self.deck:
+            return
+
+        self.hand.append(self.deck.pop(0))
+
+    def play(self, card):
+        self.hand.remove(card)
+        self.discard += self.hand
+
+
 
 
 if __name__ == "__main__":
