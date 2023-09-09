bcdbae83c71623e20b280a68b57d299d66cea7d0
Antoine Gruzelle <antogruz@gmail.com>
2023-09-09 15:38:20 +0200

    Remove UserFrame

--- beau/raceLayout.py
+++ beau/raceLayout.py
@@ -7,20 +7,16 @@ from frames import Frames
 class RaceTester(VisualTester):
     def testLayout(self):
         layout = RaceLayout(self.frame, 2)
-        labels = ["User Frame", "Track Frame", "Event Frame", "Deck1", "Deck2"]
-        frames = [layout.getUserFrame(), layout.getTrackFrame(), layout.getEventFrame()] + layout.getDecksFrames()
+        labels = ["Track Frame", "Event Frame", "Deck1", "Deck2"]
+        frames = [layout.getTrackFrame(), layout.getEventFrame()] + layout.getDecksFrames()
         for label, frame in zip(labels, frames):
             tk.Label(frame, text = label, highlightthickness = 1, highlightbackground = "black").pack()
 
 class RaceLayout():
     def __init__(self, window, decksCount):
         framesFactory = Frames(window)
-        self.user = framesFactory.new()
         self.track, self.event = framesFactory.newLine(2)
         self.decks = framesFactory.newLine(decksCount)
-
-    def getUserFrame(self):
-        return self.user
 
     def getTrackFrame(self):
         return self.track
