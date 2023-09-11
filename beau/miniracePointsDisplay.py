5e619b7b22a7c066406c3c057255780f54c4f6e3
Antoine Gruzelle <antogruz@gmail.com>
2023-09-11 15:00:33 +0200

    Add visual test for minirace display

--- beau/miniracePointsDisplay.py
+++ beau/miniracePointsDisplay.py
@@ -10,5 +10,22 @@ class MiniRacePointsDisplay:
     def displayOnTrack(self):
         if not self.observer.prizeGiver.points:
             return []
-        return [SquareDisplay(self.observer.lastSquare, 2, self.color, self.observer.prizeGiver.points[0])]
+        return [ SquareDisplay(self.observer.lastSquare + i, 2, self.color, self.observer.prizeGiver.points[0]) for i in range(2) ]
+
+from visualtests import *
+from display import RoadDisplay
+from meilleurGrimpeurObserver import createClimberObserver
+from track import Track
+class MiniRaceDisplayTester(VisualTester):
+    def testBoth(self):
+        track = Track([(5, "start"), (12, "normal"), (9, "ascent"), (12, "normal"), (5, "end")])
+        rd = RoadDisplay(self.frame, track)
+        rd.addRoadDecorator(MiniRacePointsDisplay(createClimberObserver(15, [1]), "green"))
+        rd.addRoadDecorator(MiniRacePointsDisplay(createClimberObserver(25, [5]), "red"))
+        rd.endOfTurnUpdate()
+
+
+
+if __name__ == "__main__":
+    runVisualTestsInWindow(MiniRaceDisplayTester)
 
