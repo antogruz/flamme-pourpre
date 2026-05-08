234976d16d7a6b387c99c42050616db640db9940
Antoine Gruzelle <antogruz@gmail.com>
2026-05-08 23:04:16 +0200

    Compute all energies at the same time

--- jeu/race.py
+++ jeu/race.py
@@ -31,11 +31,11 @@ class Race():
         moves = {}
         for team in self.teamsInRace:
             moves.update(team.pickNextMoves())
+        energies = { r: r.personnage.energyRules.energyFromCard(moves[r]) for r in self.riders }
 
         for r in headToTail(self.riders):
             start = r.position()
-            energy = r.personnage.energyRules.energyFromCard(moves[r])
-            r.move(energy, self.track, self.obstacles)
+            r.move(energies[r], self.track, self.obstacles)
             for observer in self.observers:
                 observer.onRiderMove(r, start, r.position(), self.obstacles, moves[r])
 
