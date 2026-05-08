d0e68c7fe9d92da0c782bb68ceec8d082276cd9e
Antoine Gruzelle <antogruz@gmail.com>
2026-05-09 00:11:14 +0200

    Add last talent of rouleur

---
+++ specialTour/talents/rouleRoule.py
@@ -0,0 +1,20 @@
+class RouleRoule:
+    def applyTo(self, personnage):
+        personnage.propulsor.cards.deck.append(7)
+
+    def displayRule(self):
+        return "Roule, Roule: Ajoutez une carte 7 à votre deck de départ"
+
+from unittests import assert_equals, runTests
+from jeu.riderBuilder import RiderBuilder
+
+class RouleRouleTest:
+    def testRouleRouleAdds7ToDeck(self):
+        rb = RiderBuilder()
+        rb.buildDeck([3, 4, 5, 6, 7] * 3)
+        personnage = rb.getResult()
+        personnage.gainTalent(RouleRoule())
+        assert_equals(4, personnage.propulsor.cards.deck.count(7))
+
+if __name__ == "__main__":
+    runTests(RouleRouleTest())
