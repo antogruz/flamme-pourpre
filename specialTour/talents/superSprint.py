0d826c24a2d47cb7f6d06942ef869b84455b07ce
Antoine Gruzelle <antogruz@gmail.com>
2026-05-23 08:17:39 +0200

    Use energyRule for superSprint

--- specialTour/talents/superSprint.py
+++ specialTour/talents/superSprint.py
@@ -1,14 +1,18 @@
 from talent import Talent
+from jeu.energyRules import EnergyRules
 
 class SuperSprint(Talent):
     def applyTo(self, personnage):
-        personnage.addBonusRule(SuperSprintBonus())
+        personnage.energyRules = SuperSprintEnergyRules(personnage.energyRules)
 
     def displayRule(self):
         return "Super Sprint: Vos 9 font 11"
 
-class SuperSprintBonus:
-    def bonusFor(self, card, rider, snapshot):
+class SuperSprintEnergyRules(EnergyRules):
+    def __init__(self, base):
+        self.base = base
+
+    def energyFromCard(self, card):
         if card == 9:
-            return 2
-        return 0
+            return 11
+        return self.base.energyFromCard(card)
